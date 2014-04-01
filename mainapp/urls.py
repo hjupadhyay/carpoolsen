68c39a1a43ffd54b58964be718a6636145606d0f
kAbhi <sharmabhishek134@gmail.com>
2014-04-01 21:17:21 +0530

    invite commented

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -32,9 +32,9 @@ urlpatterns = patterns('',
     url(r'^change_pass/', views.change_pass, name = 'change_pass'),
     url(r'^forgot_pass/', views.forgot_pass, name = 'forgot_pass'),
 
-    url(r'^invite/', views.invite, name='invite'),
+    #url(r'^invite/', views.invite, name='invite'),
 
-    url(r'^invite/', views.invite, name='invite'),
+    #url(r'^invite/', views.invite, name='invite'),
     url(r'^settings_page/', views.settings_page, name = 'settings_page'),
 
     
@@ -55,7 +55,7 @@ urlpatterns = patterns('',
     url(r'^reset_pass_page/', views.reset_pass_page, name = 'reset_pass_page'),
     url(r'^change_pass_page/', views.change_pass_page, name = 'change_pass_page'),
 
-    url(r'^invite_page/', views.invite_page, name='invite_page'),
+    #url(r'^invite_page/', views.invite_page, name='invite_page'),
 
     url(r'^pref_page/', views.pref_page, name = 'pref_page'),
     
